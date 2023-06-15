ca73919181cd0f48a9b26bfd8d8a98cb61f46e5e
Julio <julianoandradeo10@gmail.com>
2023-06-15 13:44:08 -0300

    Pronto

---
+++ script.js
@@ -0,0 +1,10 @@
+function desvia(t) {
+    const btn = t;
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(20, 80);
+}
+
+function geraPosicao(min, max) {
+    return Math.random() * (max - min) + min + "%";
+}
